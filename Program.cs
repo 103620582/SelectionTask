29f8fa5363ffbdc87ed62a05369e9bae13192066
Joy Ambrose <joyambrose@Joys-MacBook-Pro.local>
2021-03-11 17:54:17 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
